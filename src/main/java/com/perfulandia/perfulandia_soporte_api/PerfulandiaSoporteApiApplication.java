be4b038f28262757cb866a260904eb7dd396a0cb
Andru Alarcon <idazhdz@gmail.com>
2025-05-27 22:57:54 -0400

    Commit inicial del proyecto

---
+++ src/main/java/com/perfulandia/perfulandia_soporte_api/PerfulandiaSoporteApiApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_soporte_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaSoporteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaSoporteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/perfulandia_soporte_api/PerfulandiaSoporteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_soporte_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaSoporteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
